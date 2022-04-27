e602f50b66026d1b8216e2f80547f24a92616960
Robert Tortorelli <Robert.Tortorelli@Gmail.com>
2022-04-26 20:04:39 -0400

    Test

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,9 +1,19 @@
 // ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello World!\n";
+}
+
+void processString(const char* input)
+{
+    char* buffer = (char*)malloc(strlen(input));
+
+    strcpy(buffer, input);
+
 }
