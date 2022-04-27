689586f794da737f1ceab1717711bb4d10f0d788
Robert Tortorelli <Robert.Tortorelli@Gmail.com>
2022-04-26 21:26:39 -0400

    Test

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,12 @@
+// Visual Studio Console Application template.
 // ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
 
-#define _CRT_SECURE_NO_WARNINGS
+// _CRT_SECURE_NO_WARNINGS is neccessary to allow a successful build by suppressing the build error "error C4996: 'strcpy': This function or variable may be unsafe. Consider using strcpy_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS."
+// The GitHub CodeQL workflow identifies this error in this program's processString() function and produces the following security alert:
+// "No space for zero terminator.
+// This rule identifies calls to malloc that call strlen to determine the required buffer size, but do not allocate space for the zero terminator."
+//#define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
 
