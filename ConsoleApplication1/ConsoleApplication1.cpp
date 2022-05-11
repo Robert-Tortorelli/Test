96849f660884c16b0a44a3314291229f6e7ed0dc
Robert-Tortorelli <65745002+Robert-Tortorelli@users.noreply.github.com>
2022-05-10 20:45:01 -0400

    Comments added

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,7 +3,7 @@
 //
 
 // _CRT_SECURE_NO_WARNINGS is neccessary to allow a successful build by suppressing the build error "error C4996: 'strcpy': This function or variable may be unsafe. Consider using strcpy_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS."
-// The GitHub CodeQL workflow identifies this error in this program's processString() function and produces the following security alert:
+// The GitHub CodeQL workflow should identify this error in this program's processString() function and produce the following security alert:
 // "No space for zero terminator.
 // This rule identifies calls to malloc that call strlen to determine the required buffer size, but do not allocate space for the zero terminator."
 //#define _CRT_SECURE_NO_WARNINGS
@@ -19,6 +19,6 @@ void processString(const char* input)
 {
     char* buffer = (char*)malloc(strlen(input));
 
-    strcpy(buffer, input);
+    strcpy(buffer, input);                                  // This statement intentionally contains a security issue to test that the GitHub CodeQL workflow is working and produces the aforementioned security alert.
 
 }
